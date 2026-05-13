62c40afb15a160ae4666d9e3b23e8f96eec824ea
MathCactus <51454786+MathCactus@users.noreply.github.com>
2026-05-13 15:48:57 +0200

    Update password_crypto.py

--- app/password_crypto.py
+++ app/password_crypto.py
@@ -2,6 +2,7 @@ import base64
 import os
 from getpass import getpass
 
+
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives import hashes
 from cryptography.fernet import Fernet
